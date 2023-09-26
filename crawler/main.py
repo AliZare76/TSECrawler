23601e4e2c71553e2f451d9e33d936bc95768a71
alizare76 <mohammadalizare1997@gmail.com>
2023-09-26 13:45:04 +0330

    refactor with controller

--- crawler/main.py
+++ crawler/main.py
@@ -1,27 +1,28 @@
 from crawler import Crawler
 from service import DatabaseManager
 import pandas as pd
+from controller import Interface
 
-# assign the arguments
-base_url = 'http://cdn.tsetmc.com/api/Shareholder'
-start_date = '2023-09-01'
-end_date = '2023-09-20'
+# initialize interface for crawl
+controller = Interface()
+controller.assign(base_url='http://cdn.tsetmc.com/api/Shareholder',
+                  start_date = '2023-09-01', end_date = '2023-09-20')
 
 # Read the symbol IDs from the CSV file
-symbol_ids_df = pd.read_csv('./TSE Symbols.csv')
+symbol_ids_df = pd.read_csv('TSE Symbols.csv')
 symbol_ids = symbol_ids_df['id'].tolist()
+controller.url_patterns(symbol_ids)
 
 # creat a new instance of Scraper and pass the arguments
-scraper = Crawler(base_url= base_url)
-scraper.crawl_data(symbol_ids, start_date, end_date)
+controller.crawl()
 
 # final data 
-scraped_data = scraper.get_collected_data()
-scraped_data.to_csv('./collected_data.csv')
+controller.to_csv()
 
 # data schema
 columns = ['cIsin VARCHAR(80)', 'change VARCHAR(80)', 'changeAmount VARCHAR(80)', 'dEven VARCHAR(80)', 'numberOfShares VARCHAR(80)',
             'perOfShares VARCHAR(80)', 'shareHolderID VARCHAR(80)', 'shareHolderName VARCHAR(80)', 'shareHolderShareID VARCHAR(80)', 'symbol_id VARCHAR(80)']
+controller.schema(columns)
 
 # setting up the database and inserting the data
 postgres = DatabaseManager('localhost', 'postgres', user='postgres', password='123456')
