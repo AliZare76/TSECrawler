499ec9a43860728aa5b38a9746d5b17c114c19cf
alizare76 <mohammadalizare1997@gmail.com>
2023-09-26 15:57:45 +0330

    add constant values to config file

---
+++ crawler/config.py
@@ -0,0 +1,14 @@
+# constants
+base_url='http://cdn.tsetmc.com/api/Shareholder'
+
+start_date = '2023-09-01'
+
+end_date = '2023-09-20'
+
+columns = ['cIsin VARCHAR(80)', 'change VARCHAR(80)', 'changeAmount VARCHAR(80)', 'dEven VARCHAR(80)', 'numberOfShares VARCHAR(80)',
+            'perOfShares VARCHAR(80)', 'shareHolderID VARCHAR(80)', 'shareHolderName VARCHAR(80)', 'shareHolderShareID VARCHAR(80)', 'symbol_id VARCHAR(80)']
+
+db_server = 'localhost'
+db_name = 'postgres'
+db_user = 'postgres'
+db_password = '123456'
